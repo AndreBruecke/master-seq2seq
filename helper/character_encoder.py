1d1e99d72e4961d78396fa57765b4b90ddf12c43
AndreBruecke <andre.bruecke@googlemail.com>
2023-01-19 17:26:48 +0100

    Added encoding,; preprocessing functions

--- helper/character_encoder.py
+++ helper/character_encoder.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+from typing import Iterable
 
 charsets = {
     'simple': sorted(list(' \',-.abcdefghijklmnopqrstuvwxyz\t\n'))
@@ -8,10 +11,19 @@ class DictionaryCharacterEncoder:
 
     def __init__(self, charset='simple'):
         self.charset = charsets[charset] + ['§']
-        print(self.charset)
+        self.char_index = dict([(char, i) for i, char in enumerate(self.charset)])
 
-    def encode(self, text, insert_markers=False):
-        pass
+    def encode(self, texts: Iterable, insert_markers=False, shift=False):
+        max_seq_length = max([len(t) for t in texts])
+        encoded_data = np.zeros((len(texts), max_seq_length, len(self.charset)), dtype="float32")
+
+        for i, text in enumerate(texts):
+            if insert_markers:
+                text = '\t' + text + '\n'
+            for j, char in enumerate(text):
+                if not shift or j > 0:
+                    encoded_data[i, j - (1 if shift else 0), self.char_index[char]] = 1.0
+            encoded_data[i, j + (0 if shift else 1):, self.char_index[' ']] = 1.0
 
     def decode(self):
         pass
---
+++ helper/preprocessing.py
@@ -0,0 +1,12 @@
+import unicodedata as ud
+from unidecode import unidecode
+
+latin_letters = {}
+
+def is_latin(uchr):
+    try: return latin_letters[uchr]
+    except KeyError:
+         return latin_letters.setdefault(uchr, 'LATIN' in ud.name(uchr))
+
+def only_roman_chars(unistr):
+    return all(is_latin(uchr) for uchr in unistr if uchr.isalpha())
