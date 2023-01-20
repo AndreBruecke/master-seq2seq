9ee598a4fc993d32d3103ddfeaf4abe004761cad
AndreBruecke <andre.bruecke@googlemail.com>
2023-01-20 17:32:27 +0100

    Added prediction helper

--- helper/character_encoder.py
+++ helper/character_encoder.py
@@ -9,26 +9,35 @@ charsets = {
 
 class DictionaryCharacterEncoder:
 
-    def __init__(self, charset='simple'):
-        self.charset = charsets[charset] + ['§']
-        self.char_index = dict([(char, i) for i, char in enumerate(self.charset)])
+    def __init__(self, charset='simple', max_seq_length=100):
+        self.charset = charsets[charset] + ['§']  # Adds a special symbol, to which all unknown characters are mapped to during encoding.
+        self.char_index = dict([(char, i) for i, char in enumerate(self.charset)])  # Dictionary mapping characters to their indices.
+        self.inverse_char_index = dict((i, char) for char, i in self.char_index.items())
+        self.max_seq_length = max_seq_length
 
-    def encode(self, texts: Iterable, insert_markers=False, shift=False):
-        max_seq_length = max([len(t) for t in texts]) + (2 if insert_markers else 0)
-        encoded_data = np.zeros((len(texts), max_seq_length, len(self.charset)), dtype="float32")
+    def encode(self, sequences: Iterable, insert_markers=False, shift=False):
+        """_Encodes a collection of sequences using character-level one-hot encoding._
 
-        for i, text in enumerate(texts):
+        Arguments:
+            sequences -- _Iterable input sequences._
+            insert_markers -- _Whether sequences should to be wrapped with start/end markers._ (default: {False})
+            shift -- _Whether sequences should be shifted by one character._ (default: {False})
+
+        Returns:
+            _Numpy array of shape (NUMBER_OF_SEQUENCES, MAX_SEQUENCE_LENGTH, CHARSET_SIZE)_
+        """
+        encoded_data = np.zeros((len(sequences), self.max_seq_length, len(self.charset)), dtype="float32")
+
+        for i, text in enumerate(sequences):
             if insert_markers:
                 text = '\t' + text + '\n'
             for j, char in enumerate(text):
                 if not shift or j > 0:
-                    encoded_data[i, j - (1 if shift else 0), self.char_index[char]] = 1.0
+                    char_pos = self.char_index[char] if char in self.char_index else self.char_index['§']
+                    encoded_data[i, j - (1 if shift else 0), char_pos] = 1.0
             encoded_data[i, j + (0 if shift else 1):, self.char_index[' ']] = 1.0
         
         return encoded_data
-
-    def decode(self):
-        pass
 
 
 DictionaryCharacterEncoder()
---
+++ helper/prediction.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+from tensorflow import keras
+from character_encoder import DictionaryCharacterEncoder
+
+
+def predict_sequence(dce: DictionaryCharacterEncoder, encoder_model: keras.Model, decoder_model: keras.Model, input_seq):
+    # Encode the input as state vectors.
+    states_value = encoder_model.predict(input_seq, verbose=0)
+
+    # Generate empty target sequence of length 1.
+    target_seq = np.zeros((1, 1, len(dce.charset)))
+    # Populate the first character of target sequence with the start character.
+    target_seq[0, 0, dce.char_index['\t']] = 1.0
+
+    # Sampling loop for a batch of sequences
+    # (to simplify, here we assume a batch of size 1).
+    stop_condition = False
+    decoded_sentence = ""
+    while not stop_condition:
+        output_tokens, h, c = decoder_model.predict([target_seq] + states_value, verbose=0)
+
+        # Sample a token
+        sampled_token_index = np.argmax(output_tokens[0, -1, :])
+        sampled_char = dce.inverse_char_index[sampled_token_index]
+        decoded_sentence += sampled_char
+
+        # Exit condition: either hit max length
+        # or find stop character.
+        if sampled_char == "\n" or len(decoded_sentence) > dce.max_seq_length:
+            stop_condition = True
+
+        # Update the target sequence (of length 1).
+        target_seq = np.zeros((1, 1, len(dce.charset)))
+        target_seq[0, 0, sampled_token_index] = 1.0
+
+        # Update states
+        states_value = [h, c]
+    return decoded_sentence
