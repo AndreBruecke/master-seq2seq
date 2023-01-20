3a1e6be11d8327f35820bbd81734a6c3051b1b66
AndreBruecke <andre.bruecke@googlemail.com>
2023-01-20 18:29:52 +0100

    Update __init__.py

--- helper/__init__.py
+++ helper/__init__.py
@@ -1,3 +1,3 @@
-from character_encoder import DictionaryCharacterEncoder
-from prediction import predict_sequence
-from preprocessing import only_roman_chars
+from .character_encoder import DictionaryCharacterEncoder
+from .prediction import predict_sequence
+from .preprocessing import only_roman_chars
