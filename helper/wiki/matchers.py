e3b00b0000821966260a385d557730be30bc8b31
AndreBruecke <andre.bruecke@googlemail.com>
2023-03-10 21:40:18 +0100

    Implemented chunk merging

--- helper/wiki/matchers.py
+++ helper/wiki/matchers.py
@@ -6,5 +6,12 @@ def match_alphabetical_film_categories(title: str):
         return False
     return re.match(r'List of films: ([A-Z](-[A-Z])*|numbers)', title)
 
-def match_lists_of_lists(title: str):
-    return title.startswith('Lists of')
+def match_list_of_people_categories(title: str):
+    if 'Lists of' not in title:
+        return False
+    return any([term in title.lower() for term in ['people', ' male ', ' female ', ' men', ' women', ' actors', ' actresses', ' politicians']])
+
+def match_list_of_people_pages(title: str):
+    if not title.startswith('List of'):
+        return False
+    # TODO
--- helper/wiki/wiki_index_handler.py
+++ helper/wiki/wiki_index_handler.py
@@ -8,10 +8,13 @@ import pandas as pd
 import os
 import re
 import shutil
+import xml.etree.ElementTree as ET
 
 from matchers import *
 from typing import Callable
 from unidecode import unidecode
+
+from xml.etree.ElementTree import Element
 
 
 def split_index_file(index_file_path: str, out_folder: str, skip_single_character_titles: bool = True):
@@ -60,7 +63,8 @@ def search_chunks(index_file_path: str, offset_list_path: str, matches: Callable
                 matching_byte_offsets.append(int(vals[0]))
     matching_chunks = [[matching_byte_offsets[0]]]
     for i, next_offset in enumerate(matching_byte_offsets[1:]):
-        if next_offset - matching_chunks[-1][-1] > 100000000:   # More than 100 MB between offsets
+        # if next_offset - matching_chunks[-1][-1] > 100000000:   # More than 100 MB between offsets
+        if next_offset - matching_chunks[-1][-1] > 10000000:   # More than 10 MB between offsets
             matching_chunks.append([next_offset])
         else:
             matching_chunks[-1].append(next_offset)
@@ -72,7 +76,7 @@ def search_chunks(index_file_path: str, offset_list_path: str, matches: Callable
     return chunk_boundaries
 
 
-def load_chunks(mulitstream_path: str, out_folder: str, chunks: dict):
+def save_chunks(mulitstream_path: str, out_folder: str, chunks: dict):
     with open(mulitstream_path, 'rb') as wiki_f:
         for i, chunk in enumerate(chunks):
             wiki_f.seek(chunk['start'])
@@ -82,12 +86,40 @@ def load_chunks(mulitstream_path: str, out_folder: str, chunks: dict):
             with bz2.BZ2File(os.path.join(out_folder, f'{i}-temp.bz2')) as fr, open(os.path.join(out_folder, f'{i}.xml'), 'wb') as fw:
                 shutil.copyfileobj(fr, fw)
 
+def merge_chunks(chunk_folder: str, out_path: str, matches: Callable):
+    chunk_files = [f for f in os.listdir(chunk_folder) if re.fullmatch(r'\d+\.xml', f)]
+    matching_pages = []
+    for chunk_file in chunk_files:
+        xml = ''
+        with open(os.path.join(chunk_folder, chunk_file), 'r', encoding='utf-8') as chunk_f:
+            xml = chunk_f.read().strip()
+        xml = re.split(r'</?page>', xml)
+        for page_content in xml:
+            title_elem = re.search(r'<title>(.*?)</title>', page_content)
+            if title_elem is None:
+                continue
+            title = title_elem.group(1).strip()
+            if not matches(title):
+                continue
+            page_content = page_content.replace('<page>', '')
+            matching_pages.append('<page>' + page_content.strip() + '</page>')
+    with open(out_path, 'w', encoding='utf-8') as merged_f:
+        merged_f.write('<root>\n\n')
+        merged_f.write('\n\n'.join(matching_pages))
+        merged_f.write('\n\n</root>')
+
+    
 
 # split_index_file('D:/HDa/Thesis/Repos/master-seq2seq/large_data/wiki_db_en/enwiki-20211020-pages-articles-multistream-index.txt', 'D:/HDa/Thesis/Repos/master-seq2seq/large_data/wiki_db_en/')
 # write_unique_offsets('D:/HDa/Thesis/Repos/master-seq2seq/large_data/wiki_db_en/enwiki-20211020-pages-articles-multistream-index.txt', 'D:/HDa/Thesis/Repos/master-seq2seq/large_data/wiki_db_en/')
 
 # chunk_bounds = search_chunks('T:/MasterData/wikipedia_dump/index_split/enwiki-20211020-index__L.txt', 'T:/MasterData/wikipedia_dump/index_split/enwiki-20211020-index___OFFSETS.txt', match_alphabetical_film_categories)
-# load_chunks('T:/MasterData/wikipedia_dump/enwiki-20211020-pages-articles-multistream.xml.bz2', 'T:/MasterData/wikipedia_dump/xml_film_lists/', chunk_bounds)
+# save_chunks('T:/MasterData/wikipedia_dump/enwiki-20211020-pages-articles-multistream.xml.bz2', 'T:/MasterData/wikipedia_dump/m_alphabetical_film_categories/', chunk_bounds)
 
-chunk_bounds = search_chunks('T:/MasterData/wikipedia_dump/index_split/enwiki-20211020-index__L.txt', 'T:/MasterData/wikipedia_dump/index_split/enwiki-20211020-index___OFFSETS.txt', match_lists_of_lists)
-load_chunks('T:/MasterData/wikipedia_dump/enwiki-20211020-pages-articles-multistream.xml.bz2', 'T:/MasterData/wikipedia_dump/xml_lists_of_lists/', chunk_bounds)
+# chunk_bounds = search_chunks('T:/MasterData/wikipedia_dump/index_split/enwiki-20211020-index__C.txt', 'T:/MasterData/wikipedia_dump/index_split/enwiki-20211020-index___OFFSETS.txt', match_list_of_people_categories)
+# save_chunks('T:/MasterData/wikipedia_dump/enwiki-20211020-pages-articles-multistream.xml.bz2', 'T:/MasterData/wikipedia_dump/m_list_of_people_categories/', chunk_bounds)
+# merge_chunks('T:/MasterData/wikipedia_dump/m_list_of_people_categories/', 'T:/MasterData/wikipedia_dump/m_list_of_people_categories/pages.xml', match_list_of_people_categories)
+
+# chunk_bounds = search_chunks('T:/MasterData/wikipedia_dump/index_split/enwiki-20211020-index__L.txt', 'T:/MasterData/wikipedia_dump/index_split/enwiki-20211020-index___OFFSETS.txt', match_list_of_people_pages)
+# save_chunks('T:/MasterData/wikipedia_dump/enwiki-20211020-pages-articles-multistream.xml.bz2', 'T:/MasterData/wikipedia_dump/m_list_of_people_pages/', chunk_bounds)
+# merge_chunks('T:/MasterData/wikipedia_dump/m_list_of_people_pages/', 'T:/MasterData/wikipedia_dump/m_list_of_people_pages/pages.xml', match_list_of_people_pages)
